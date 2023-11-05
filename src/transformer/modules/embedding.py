179bd48a5ae4e56cac69a2430df3b6835b73e2a6
yngtdd <yngtdd@gmail.com>
2023-11-05 14:53:13 -0500

    feat: Add input embeddings

---
+++ src/transformer/modules/embedding.py
@@ -0,0 +1,23 @@
+import math
+import torch.nn as nn
+
+
+class InputEmbedding(nn.Module):
+    """Transformer Input Embedding"""
+
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        """Embed our tokenized inputs
+        
+        Note:
+            Following section 3.4 in "Attention is All You Need",
+            we multiply the embeddings by the square root of the 
+            model's dimension
+        """
+        return self.embedding(x) * math.sqrt(self.d_model)
+
