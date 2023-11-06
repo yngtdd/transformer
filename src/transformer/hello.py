b5d61ff36defeef1be6fed232dd44fdb154ec191
yngtdd <yngtdd@gmail.com>
2023-11-06 16:53:37 -0500

    refactor: Remove hello world starter

--- src/transformer/hello.py
+++
@@ -1,7 +0,0 @@
-def hello(name: str) -> str:
-    """Greet a person
-
-    Args:
-        name: the persone to greet
-    """
-    return f"Hello, {name}!"
--- tests/test_hello.py
+++
@@ -1,5 +0,0 @@
-from transformer.hello import hello
-
-
-def test_hello(default_person):
-    assert hello(default_person) == "Hello, World!"
