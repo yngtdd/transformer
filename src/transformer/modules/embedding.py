9ca7c2361497a8c99c89b33956cf3f2721037b57
yngtdd <yngtdd@gmail.com>
2023-11-06 08:48:32 -0500

    refactor: Rename embedding
    Both the input and output embeddings can use the same module.

--- src/transformer/modules/embedding.py
+++ src/transformer/modules/embedding.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 
 
-class InputEmbedding(nn.Module):
+class Embedding(nn.Module):
 
     def __init__(self, d_model: int, vocab_size: int):
         """Transformer Input Embedding
